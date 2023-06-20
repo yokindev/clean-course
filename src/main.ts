3ea658555d8f5dff3b6ef546bb62f3a0464ca868
YokinDev <yokindev@gmail.com>
2023-06-20 11:22:34 +0200

    add clean-code file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-import './style.css'
+import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
-`
-
+`;
